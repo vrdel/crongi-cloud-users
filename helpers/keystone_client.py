67b1d56af774670ec072a8bf3bda03184fb2d373
Daniel Vrcic <daniel.vrcic@gmail.com>
2019-10-07 21:08:20 +0200

    assign user to project by granting member role on project

--- helpers/keystone_client.py
+++ helpers/keystone_client.py
@@ -8,6 +8,10 @@ from keystoneauth1 import session
 import argparse
 
 
+def get_role(client, name):
+    found = filter(lambda r: r.name == name, client.roles.list())
+    return found[0] if found else None
+
 def project_create(client, name):
     new = client.projects.create(name, 'default')
     return new
@@ -35,6 +39,7 @@ def main():
     parser.add_argument('--admin-project', required=True, dest='project')
     parser.add_argument('--admin-projectid', required=True, dest='projectid')
     parser.add_argument('--admin-url', required=True, dest='url')
+    parser.add_argument('--member-role', required=True, dest='memberrole')
 
     parser.add_argument('--new-user', dest='newuser')
     parser.add_argument('--new-project', dest='newproject')
@@ -48,19 +53,26 @@ def main():
                        project_domain_id='Default',
                        project_name=args.project)
     sess = session.Session(auth=auth)
-    cl = client.Client(session=sess, interface='public')
+    admin_client = client.Client(session=sess, interface='public')
 
     if args.newproject:
-        project = project_exist(cl, args.newproject)
+        project = project_exist(admin_client, args.newproject)
         if project:
             print('Project exists {0}'.format(project.name))
-            user = user_exist(cl, args.newuser)
+            user = user_exist(admin_client, args.newuser)
             if user:
                 print('User exists {0}'.format(user.name))
             else:
-                print(user_create(cl, args.newuser, project))
+                member_role = get_role(admin_client, args.memberrole)
+                newuser = user_create(admin_client, args.newuser, project)
+                admin_client.roles.grant(member_role, user=newuser, project=project)
+                print('User {0} assigned to project {1}'.format(newuser.name, project.name))
         else:
-            newproject = project_create(cl, args.newproject)
-            print(user_create(cl, args.newuser, newproject))
+            newproject = project_create(admin_client, args.newproject)
+            newuser = user_create(admin_client, args.newuser, newproject)
+            member_role = get_role(admin_client, args.memberrole)
+            admin_client.roles.grant(member_role, newuser, newproject)
+            print('User {0} assigned to project {1}'.format(newuser.name, newproject.name))
+
 
 main()
