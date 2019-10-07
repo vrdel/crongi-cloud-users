e9b177e5cb375bd837f2448149602e7c48e4daaf
Daniel Vrcic <daniel.vrcic@gmail.com>
2019-10-07 14:10:20 +0200

    project and users exists methods

--- helpers/keystone_client.py
+++ helpers/keystone_client.py
@@ -7,14 +7,24 @@ from keystoneauth1 import session
 
 import argparse
 
-def main():
+def user_exist(client, user):
+    found = filter(lambda u: u.name == user, client.users.list())
+    return found[0] if found else None
 
+def project_exist(client, project):
+    found = filter(lambda p: p.name == project, client.projects.list())
+    return found[0] if found else None
+
+def main():
     parser = argparse.ArgumentParser(description="crongi-cloud-users client")
-    parser.add_argument('--keystone-user', required=True, dest='user')
-    parser.add_argument('--keystone-password', required=True, dest='password')
-    parser.add_argument('--keystone-project', required=True, dest='project')
-    parser.add_argument('--keystone-projectid', required=True, dest='projectid')
-    parser.add_argument('--keystone-url', required=True, dest='url')
+    parser.add_argument('--admin-user', required=True, dest='user')
+    parser.add_argument('--admin-password', required=True, dest='password')
+    parser.add_argument('--admin-project', required=True, dest='project')
+    parser.add_argument('--admin-projectid', required=True, dest='projectid')
+    parser.add_argument('--admin-url', required=True, dest='url')
+
+    parser.add_argument('--new-user', dest='newuser')
+    parser.add_argument('--new-project', dest='newproject')
     args = parser.parse_args()
 
     auth = v3.Password(auth_url=args.url,
@@ -25,10 +35,14 @@ def main():
                        project_domain_id='Default',
                        project_name=args.project)
     sess = session.Session(auth=auth)
-    print(sess.get_token())
-    print(sess.get_user_id())
     cl = client.Client(session=sess, interface='public')
-    print(cl.projects.list())
-    print(cl.users.list())
+
+    if args.newproject:
+        project = project_exist(cl, args.newproject)
+        if project:
+            print(project.name)
+            user = user_exist(cl, args.newuser)
+            if user:
+                print(user.name)
 
 main()
