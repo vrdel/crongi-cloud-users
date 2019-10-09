3b4408e5363ab646a3beb951f7aa9178682ddae9
Daniel Vrcic <daniel.vrcic@gmail.com>
2019-10-09 10:20:01 +0200

    remove syslog logging

--- modules/log.py
+++ modules/log.py
@@ -5,9 +5,6 @@ import os.path
 
 
 class Logger(object):
-    """
-       Logger objects with initialized File and Syslog logger.
-    """
     logger = None
 
     def _init_stdout(self):
@@ -17,16 +14,6 @@ class Logger(object):
 
         logging.basicConfig(format=lfs, level=lv, stream=sys.stdout)
         self.logger = logging.getLogger(self._caller)
-
-    def _init_syslog(self):
-        lfs = '%(name)s[%(process)s]: %(levelname)s ' + self._caller + ' - %(message)s'
-        lf = logging.Formatter(lfs)
-        lv = logging.INFO
-
-        sh = logging.handlers.SysLogHandler('/dev/log', logging.handlers.SysLogHandler.LOG_USER)
-        sh.setFormatter(lf)
-        sh.setLevel(lv)
-        self.logger.addHandler(sh)
 
     def __init__(self, caller):
         self._caller = os.path.basename(caller)
