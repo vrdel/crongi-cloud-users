4a738e2d61ff53b096942d5aeac7b72c349e0ee0
Daniel Vrcic <daniel.vrcic@gmail.com>
2019-10-11 22:37:28 +0200

    filter interested fields from projects API fetch

--- modules/external.py
+++ modules/external.py
@@ -3,6 +3,9 @@ import json
 
 
 class ProjectFeed(object):
+    interested_user_fields = ['uid']
+    interested_project_fields = ['sifra']
+
     def __init__(self, logger, url, timeout):
         try:
             response = requests.get(url, timeout=timeout, verify=False)
@@ -15,8 +18,25 @@ class ProjectFeed(object):
         except Exception as e:
             logger.error(e)
 
-    def get(self):
+    def _filtered_projects(self):
         return filter(lambda p: bool(p['htc']) and bool(p['status_id']), self.projects)
+
+    def _interested_fields(self, projects):
+        projects_trim = dict()
+        for project in projects:
+            projects_users = project['users']
+            projects_trim['users'] = dict()
+            for project_key in project.iterkeys():
+                if project_key in self.interested_project_fields:
+                    projects_trim[project_key] = project[project_key]
+            for project_user in projects_users:
+                for project_user_key in project_user.iterkeys():
+                    if project_user_key in self.interested_user_fields:
+                        projects_trim['users'][project_user_key] = project_user[project_user_key]
+        return projects_trim
+
+    def get(self):
+        return self._interested_fields((self._filtered_projects()))
 
 
 class JsonExtend(object):
